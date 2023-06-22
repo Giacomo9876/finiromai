d62369f82d3fd65d094479cb48489b5cb31c45d9
Giacomo9876 <gicomo98@gmail.com>
2023-06-22 15:07:30 +0200

    hadn thing to call

--- hand_things_to_call.py
+++ hand_things_to_call.py
@@ -8,6 +8,9 @@ from api_lol_functions import LoL
 from apscheduler.schedulers.background import BackgroundScheduler
 
 class JobToDo:
+
+    _valuedb = "tknburned"
+    _dbvalue = "storage"
     
     def shitcalling(self):
         a = LoL(name="ParmiJanna")
@@ -15,11 +18,9 @@ class JobToDo:
         d = a.get_matchid_list()
         a.check_match_id()
         print(a.amount)
-        cnx = mysql.connector.connect(user=os.getenv("USER"), password=os.getenv("PWD"),
-                              host='127.0.0.1',
-                              database='azircoin')
+        cnx = a.conn_db()
         cursor = cnx.cursor()
-        query = "SELECT tknburned FROM storage WHERE id=1;"#"INSERT INTO allstorage (alltokken) VALUES(%d);"
+        query = f"SELECT {self._valuedb} FROM {self._dbvalue} WHERE id=1;"#"INSERT INTO allstorage (alltokken) VALUES(%d);"
         cursor.execute(query,)
         # cnx.commit()
         tkn_burned = cursor.fetchall()
@@ -29,7 +30,7 @@ class JobToDo:
         print(value)
         value = value + a.amount
         print(value)
-        query = 'UPDATE storage SET tknburned = %s WHERE id = 1; '
+        query = f'UPDATE {self._dbvalue} SET {self._valuedb} = %s WHERE id = 1; '
         args = (value),
         cursor.execute(query, args)
         cnx.commit()
@@ -38,19 +39,19 @@ class JobToDo:
     def conn_web(self):
 
         scheduler = BackgroundScheduler()
-        scheduler.add_job(self.shitcalling, 'interval', seconds=30)#23h = 82800
+        scheduler.add_job(self.shitcalling, 'interval', seconds=50)#23h = 82800
         scheduler.start()
         print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
 
         try:
             # This is here to simulate application activity (which keeps the main thread alive).
             while True:
-                time.sleep(3)
+                time.sleep(30)
         except (KeyboardInterrupt, SystemExit):
             # Not strictly necessary if daemonic mode is enabled but should be done if possible
             scheduler.shutdown()
         
  
 
-a = JobToDo()
-print(a.conn_web())
+# a = JobToDo()
+# print(a.conn_web())
